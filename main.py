f382a1f7c0a7d42a23e1178f4917e05c1ef4262b
Firzy <rivaldyfirzzy@gmail.com>
2023-10-03 20:36:28 +0700

    first commit

--- main.py
+++ main.py
@@ -7,6 +7,8 @@
 # Nama_Anda = str(input("Input Nama : "))
 # print("Selamat datang " + Nim_Anda +" "+ Nama_Anda + "di perpustakaan Buddhi Dharma")
 
+
+
 # print("================")
 # print("Konversi Suhu Celsius ke Fahrenheit")
 # print("================")
@@ -80,28 +82,118 @@
 # NamaPengguna = (input("Masukan Nama Pengguna :"))
 # print("Halo " + NamaPengguna + "! Selamat datang ke Program Python sederhana")
 
-print('----------------')
-print('kasir')
-print('----------------')
 
-print(' ')
 
-HargaSusuPerBotol = 25000
-HargaBerasPerKg = 12000
-HargaTelurPerButir = 2000
-HargaSardenPerKg = 5000
+# print('----------------')
+# print('kasir')
+# print('----------------')
+#
+# print(' ')
+#
+# HargaSusuPerBotol = 25000
+# HargaBerasPerKg = 12000
+# HargaTelurPerButir = 2000
+# HargaSardenPerKg = 5000
+#
+# JumlahBotolSusu = int(input('Jumlah botol susu yang dibeli :'))
+# JumlahKgBeras = int(input('Jumlah kilogram beras yang dibeli :'))
+# JumlahTelur = int(input('Jumlah telur yang dibeli :'))
+# JumlahKalengSarden = int(input('jumlah kaleng sarden yang dibeli :'))
+#
+# TotalHargaSusu = HargaSusuPerBotol * JumlahBotolSusu
+# TotalHargaBeras = HargaBerasPerKg * JumlahKgBeras
+# TotalHargaTelur = HargaTelurPerButir * JumlahTelur
+# TotalHargaSarden = HargaSardenPerKg * JumlahKalengSarden
+#
+# TotalBelanjaan = TotalHargaSusu + TotalHargaBeras + TotalHargaTelur + TotalHargaSarden
+#
+# print('Total harga belanjaan adalah Rp.', TotalBelanjaan)
 
-JumlahBotolSusu = int(input('Jumlah botol susu yang dibeli :'))
-JumlahKgBeras = int(input('Jumlah kilogram beras yang dibeli :'))
-JumlahTelur = int(input('Jumlah telur yang dibeli :'))
-JumlahKalengSarden = int(input('jumlah kaleng sarden yang dibeli :'))
 
-TotalHargaSusu = HargaSusuPerBotol * JumlahBotolSusu
-TotalHargaBeras = HargaBerasPerKg * JumlahKgBeras
-TotalHargaTelur = HargaTelurPerButir * JumlahTelur
-TotalHargaSarden = HargaSardenPerKg * JumlahKalengSarden
 
-TotalBelanjaan = TotalHargaSusu + TotalHargaBeras + TotalHargaTelur + TotalHargaSarden
 
-print('Total harga belanjaan adalah Rp.', TotalBelanjaan)
+
+# print('--------------------------------')
+# print('Menghitung Nilai Akhir Mahasiswa')
+# print('--------------------------------')
+#
+# Nama = str(input('Masukan Nama Anda     :'))
+# Uts = int(input('Masukan Nilai UTS     :'))
+# Uas = int(input('Masukan Nilai UAS     :'))
+# Tgs = int(input('Masukan Nilai Tugas   :'))
+# Pyk = int(input('Masukan Nilai Proyek  :'))
+#
+# print('')
+#
+# print('Mahasiswa yang bernama ' + Nama)
+# print('Dengan Nilai Presentasi yang dihasilkan')
+#
+# nilaiuts = Uts*(30/100)
+# nilaiuas = Uas*(40/100)
+# nilaitugas = Tgs*(15/100)
+# nilaiproyek = Pyk*(15/100)
+# nilaiakhir = nilaiuts+nilaiuas+nilaitugas+nilaiproyek
+#
+# print('Nilai UTS * 30%    : ', nilaiuts)
+# print('Nilai UAs * 40%    : ', nilaiuas)
+# print('Nilai Tugas * 15%  : ', nilaitugas)
+# print('Nilai Proyek * 15% : ', nilaiproyek)
+#
+# print('')
+#
+# print('Nilai Akhir yang diperoleh Mahasiswa dengan Nama ' + Nama + ' (NIM: ' + Nim + ')' ':', nilaiakhir)
+
+
+
+
+# print('--------------------------------')
+# print('Menghitung Nilai Akhir Mahasiswa')
+# print('--------------------------------')
+#
+# Nama = str(input('Masukan Nama Anda     :'))
+# Nim = str(input('Masukan Nim Anda      :'))
+# BobotUts = int(input('Masukan bobot UTS (dalam persentase) :'))
+# BobotUas = int(input('Masukan bobot UAS (dalam persentase) :'))
+# BobotTugas = int(input('Masukan bobot Tugas (dalam persentase) :'))
+# BobotProyek = int(input('Masukan bobot Proyek (dalam persentase) :'))
+# Uts = int(input('Masukan Nilai UTS     :'))
+# Uas = int(input('Masukan Nilai UAS     :'))
+# Tgs = int(input('Masukan Nilai Tugas   :'))
+# Pyk = int(input('Masukan Nilai Proyek  :'))
+#
+# print('')
+#
+# print('Mahasiswa yang bernama ' + Nama + '(NIM: ', Nim + ')')
+# print('Dengan Nilai Presentasi yang dihasilkan')
+#
+# nilaiuts = Uts*(BobotUts/100)
+# nilaiuas = Uas*(BobotUas/100)
+# nilaitugas = Tgs*(BobotTugas/100)
+# nilaiproyek = Pyk*(BobotProyek/100)
+# nilaiakhir = nilaiuts+nilaiuas+nilaitugas+nilaiproyek
+#
+# print('Nilai UTS * 30%    : ', nilaiuts)
+# print('Nilai UAs * 40%    : ', nilaiuas)
+# print('Nilai Tugas * 15%  : ', nilaitugas)
+# print('Nilai Proyek * 15% : ', nilaiproyek)
+#
+# print('')
+#
+# print('Nilai Akhir yang diperoleh Mahasiswa dengan Nama ' + Nama + ' (NIM: ' + Nim + ')' ':', nilaiakhir)
+
+
+print('----------------------------------------------')
+print('Simulasi Menabung Rutin dengan bunga Berbunga')
+print('----------------------------------------------')
+
+
+JumlahYangDiTabung = int(input('Masukan jumlah awal yang akan ditabung (dalam Rupiah) :'))
+BungaTahunan = int(input('Masukan tingkat bunga tahunan (dalam persentase) :'))
+PeriodeTabungan = int(input('Masukan periode tabungan (dalam tahun) :'))
+TabunganRutinPerTahun = int(input('Masukan jumlah tabungan rutin per tahun (dalam Rupiah) :'))
+
+
+JumlahAkhir = JumlahYangDiTabung * (1 + BungaTahunan/100)**PeriodeTabungan + TabunganRutinPerTahun * PeriodeTabungan
+
+print('Setelah', PeriodeTabungan, 'jumlah uang yang akan Anda miliki adalah RP.', JumlahAkhir)
 
